a5fc59843adfade609bf51dfb55b377eaa4b6bae
Charlenechuaxinzi <63596626+Charlenechuaxinzi@users.noreply.github.com>
2023-05-03 05:43:55 +0800

    test to fix vercel deployment issue

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,23 @@
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+// import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
-export default {
-  // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
-  // for more information about preprocessors
-  preprocess: vitePreprocess(),
-}
+// export default {
+//   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
+//   // for more information about preprocessors
+//   preprocess: vitePreprocess(),
+// }
+
+import node from '@sveltejs/adapter-node';
+
+const config = {
+  kit: {
+    adapter: node(),
+    target: '#svelte',
+    vite: {
+      build: {
+        outDir: 'build'
+      }
+    }
+  }
+};
+
+export default config;
